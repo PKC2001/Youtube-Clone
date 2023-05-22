d938a321f2aad58b078a316262d8c036aff40b4a
PKC2001 <chillawarpranav@gmail.com>
2023-05-22 08:35:47 +0530

    Updates

---
+++ precache-manifest.b286e8bf746bb80c0f005bf238c3fd09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d0be6dabfdb9e28a1679b4d331fe4c",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "f0ad54b40c2304d4b87a",
+    "url": "/youtube-clone/static/css/main.103ded68.chunk.css"
+  },
+  {
+    "revision": "a0744025c362061b7d23",
+    "url": "/youtube-clone/static/js/2.aa7b638a.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-clone/static/js/2.aa7b638a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ad54b40c2304d4b87a",
+    "url": "/youtube-clone/static/js/main.949e24e9.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
